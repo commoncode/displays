3465d27e69fa18a3c6c9c500cb732a155c32ab11
Y3K <dboy.y3k@gmail.com>
2014-05-14 00:23:50 -0500

    Update Command

--- displays/management/commands/create_displays.py
+++ displays/management/commands/create_displays.py
@@ -1,4 +1,9 @@
+from random import choice
+
+from django.core.management import call_command
 from django.core.management.base import BaseCommand, CommandError
+
+from positions.models import Position
 
 from ...factories import DisplayInstanceFactory
 
@@ -9,5 +14,11 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         print "Creating Display Instances"
 
+        positions = Position.objects.all()
+
+        if not positions.exists():
+            call_command('create_positions')
+
         for i in  range(5):
-            DisplayInstanceFactory()
+            DisplayInstanceFactory(position=choice(positions))
+            print "Created Display"
